97b0a6fd52197c68cd87dc373bb507bf0017fb18
25A31A4374 <lakshmipoojasri2007@gmail.com>
2026-03-14 19:31:33 +0530

    Create salary.cpp

---
+++ salary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float basic, hra, da, total;
+
+    cout << "Enter Basic Salary: ";
+    cin >> basic;
+
+    hra = basic * 0.20;
+    da = basic * 0.10;
+
+    total = basic + hra + da;
+
+    cout << "Total Salary is: " << total << endl;
+
+    return 0;
+}
